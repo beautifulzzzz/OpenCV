3c4985cbed5ccfdc8285b3217c9a420f9b624e98
litao <953521476@qq.com>
2015-01-23 16:45:13 +0800

    solve 越来越黑的原因了
    发现越弄越黑：存成图片会把double类型的只取0~255结果,于是在外面建立变量存储叠加的像素值

--- License plate recognition/from lots of images to get background(1).c
+++ License plate recognition/from lots of images to get background(1).c
@@ -1,4 +1,4 @@
-#include "opencv2/highgui/highgui.hpp"
+﻿#include "opencv2/highgui/highgui.hpp"
 #include "opencv2/imgproc/imgproc.hpp"
 #include <iostream>
 using namespace cv;
@@ -9,14 +9,23 @@ using namespace std;
 //------------------------------------------------------------------------
 //【OpenCV应用笔记】(彩色/灰度)图像像素值读取并保存到txt文件:http://www.cnblogs.com/skyseraph/archive/2012/02/23/2365542.html
 //opencv中的IplImage http://blog.csdn.net/fenggaoyuehei/article/details/6040911
-//1-23 15:53 所有图片叠加求背景法尝试，现在正在运行中...
+//1-23 15:53 所有图片叠加求背景法尝试，现在正在运行中...Git
+//发现越弄越黑：存成图片会把double类型的只取0~255结果,于是在外面建立变量存储叠加的像素值
+class Pixel
+{
+public:
+	double v[3];
+	Pixel(){v[0]=v[1]=v[2]=0;}
+};
 void getBg(int nPicNum=1)
 {
 	char name[100];
 	IplImage* img;
-	CvScalar p1,p2;
+	CvScalar p1;
 	
 	IplImage* des=cvLoadImage("Tao1/1.jpg",-1);
+	Pixel *pixel=new Pixel[des->width*des->height];
+	memset(pixel,0,sizeof(pixel));
 
 	for(int k=2;k<=nPicNum;k++)
 	{
@@ -29,26 +38,26 @@ void getBg(int nPicNum=1)
 			for(int j=0;j<img->width;j++)
 			{
 				p1 = cvGet2D(img,i,j);
-				p2 = cvGet2D(des,i,j);
-				p2.val[0]+=p1.val[0];
-				p2.val[1]+=p1.val[1];
-				p2.val[2]+=p1.val[2];
-				cvSet2D(des,i,j,p2);
-				//cout<<p.val[0] <<" "<<p.val[1]<<" "<<p.val[2] <<" "<<endl;
+				pixel[i*img->width+j].v[0]+=p1.val[0];
+				pixel[i*img->width+j].v[1]+=p1.val[1];
+				pixel[i*img->width+j].v[2]+=p1.val[2];
 			}    
 		}
 		cvReleaseImage(&img);
-		cout<<k<<'\n';
+		/*cout<<k<<'\n';*/
 	}
 	for(int i=0;i<des->height;i++)
 	{
 		for(int j=0;j<des->width;j++)
 		{
-			p2 = cvGet2D(des,i,j);
-			p2.val[0]/=nPicNum;
-			p2.val[1]/=nPicNum;
-			p2.val[2]/=nPicNum;
-			cvSet2D(des,i,j,p2);
+			p1 = cvGet2D(des,i,j);
+			pixel[i*des->width+j].v[0]+=p1.val[0];
+			pixel[i*des->width+j].v[1]+=p1.val[1];
+			pixel[i*des->width+j].v[2]+=p1.val[2];
+			p1.val[0]=pixel[i*des->width+j].v[0]/nPicNum;
+			p1.val[1]=pixel[i*des->width+j].v[1]/nPicNum;
+			p1.val[2]=pixel[i*des->width+j].v[2]/nPicNum;
+			cvSet2D(des,i,j,p1);
 			//cout<<p.val[0] <<" "<<p.val[1]<<" "<<p.val[2] <<" "<<endl;
 		}    
 	}
@@ -56,6 +65,7 @@ void getBg(int nPicNum=1)
 	cvSaveImage(name,des);
 	/*cvShowImage("des",des);*/
 	cvReleaseImage(&des);
+	delete[] pixel;
 }
 int main()
 {
@@ -74,7 +84,7 @@ int main()
 	//
 	//imshow("src1",src);
 	//waitKey();
-	for(int i=1;i<67;i++)
+	for(int i=28;i<67;i++)
 	{
 		getBg(i);
 		cout<<i<<'\n';
